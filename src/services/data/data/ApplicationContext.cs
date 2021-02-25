c40654c5a3158b31ea983965cba74859ecb5ca47
martyniukroman <hjbfrcgq@gmail.com>
2021-02-25 19:15:37 +0200

    env(data): add ef core with postgre sql

---
+++ src/services/data/data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PostgresApp
+{
+    public class ApplicationContext : DbContext
+    {
+        
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=postgres");
+        }
+    }
+}
