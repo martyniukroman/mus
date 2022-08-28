1614714b9e1bd88d8cb191d468d5b5112bd53861
romanmartyniuk <hjbfrcgq@gmail.com>
2022-08-28 20:06:30 +0300

    dev(architecture): clean architecture approach

---
+++ domain/valueObjects/AdAccount.cs
@@ -0,0 +1,13 @@
+﻿using domain.applicationExceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.valueObjects
+{
+    public class AdAccount : ValueObject
+    {
+    }
+}
